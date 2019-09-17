f22143be1e87aab1403efc8eeb887560a301cdd0
Zachary Schulz <zjschulz@gmail.com>
2019-09-17 00:24:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 def assign_rooms
   array = []
   counter = 0
-  attendees.each {|i| array << "Hello, #{i}! You'll be assigned to room " + array.index + "!"}
-  array
-end
+  attendees.each do {|i| array << "Hello, #{i}! You'll be assigned to room " + array.index + "!"}
+    array
+  end
 end
