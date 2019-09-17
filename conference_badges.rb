cc0f84fe95032e6da77d0736c82e17e91bdc37a7
Zachary Schulz <zjschulz@gmail.com>
2019-09-17 00:01:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,11 +2,11 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator()
+def batch_badge_creator(attendees)
   
 end
 
-def assign_rooms()
+def assign_rooms(attendees)
   
 end
 
