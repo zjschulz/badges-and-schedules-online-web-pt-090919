2f7e767443e81386687b0c377f03b19d0e096760
Zachary Schulz <zjschulz@gmail.com>
2019-09-17 00:07:52 +0000

    Automatically backed up by Learn

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -15,7 +15,7 @@ describe 'conference_badges' do
                ]}
 
   let(:room_assignments) {[
-                            H"ello, Edsger! You'll be assigned to room 1!",
+                            "Hello, Edsger! You'll be assigned to room 1!",
                             "Hello, Ada! You'll be assigned to room 2!",
                             "Hello, Charles! You'll be assigned to room 3!",
                             "Hello, Alan! You'll be assigned to room 4!",
