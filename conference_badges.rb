1da3f14a402f6f8ee4191949824d9296c7ec4d6b
Zachary Schulz <zjschulz@gmail.com>
2019-09-17 00:14:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,15 +5,3 @@ end
 def batch_badge_creator(attendees)
   "Hello, my name is #{attendees}."
 end
-
-def assign_rooms(attendees)
-  array = []
-  counter = 0
-  array.each {|i| "Hello, #{i}! You'll be assigned to room #{counter}!"
-    counter +=1
-  end
-end
-
-def printer()
-  
-end
